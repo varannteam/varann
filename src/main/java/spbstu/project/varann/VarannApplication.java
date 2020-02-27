c793ce3a40b1dce78870f79a2dd896000b4f6f87
Stepan Ponomarev <49997776+step-ponomarev@users.noreply.github.com>
2020-02-27 14:16:05 +0300

    Task1 (#4)

---
+++ src/main/java/spbstu/project/varann/VarannApplication.java
@@ -0,0 +1,13 @@
+package spbstu.project.varann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarannApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VarannApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spbstu/project/varann/VarannApplicationTests.java
@@ -0,0 +1,13 @@
+package spbstu.project.varann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
